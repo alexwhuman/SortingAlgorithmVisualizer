148a188a878849e9aefb1266f0425e27529d673c
Alex <alexwhuman@gmail.co>
2019-12-03 09:05:42 +0200

    Sorting Algorithms - Generate Random
    - Changed way generate random works (Now generates random number within range, instead of array of random numbers)
    - Updated comments
    
    	modified:   scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,11 @@
 // Basic operation/planning of the sorting algorithms:
-// - GenerateRandom(int n) will generate an array of values ranging from 1 - 100, the size of the array will depend on input n
+// - GenerateRandom() will generate a random number between 1 and 100 and return that number
 // - The sorting algorithms will accept an array and sort the array, not returning a value but rather performing a specific action everytime an element is selected, compared and moved within the array (so that I can visualize the sorting using DOM elements)
 
-// Function to generate an array of specified size, containing random numbers between 1 - 100
-function generateRandom(n) {
-    var randomArray = new Array(n);
-    for (let index = 0; index < randomArray.length; index++) {
-        randomArray[index] = Math.ceil(Math.random() * 100);
-        console.log(randomArray[index]);
-    }
-    return randomArray;
+// Function to generate a random number between 1 and 100
+function generateRandom() {
+    randomNumber = Math.ceil(Math.random() * 100);
+    return randomNumber;
 }
 
 // Selection Sort
